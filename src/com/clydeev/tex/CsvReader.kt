40ed99ffc0705bd2acace85b6fcfc70b7e1ec070
clydeev <clydevelasquez@outlook.com>
2017-06-26 11:53:16 +0800

    Add additional documentations

--- src/com/clydeev/tex/CsvReader.kt
+++ src/com/clydeev/tex/CsvReader.kt
@@ -1,6 +1,9 @@
 package com.clydeev.tex
 
-import java.io.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.io.IOException
 import java.util.stream.Stream
 
 /**
@@ -10,24 +13,36 @@ import java.util.stream.Stream
  * @version 1.0
  * @since 6/6/2017
  */
-public class CsvReader
+class CsvReader
     @Throws(IOException::class)
     constructor(var file: File) {
 
     private val lineReader: BufferedReader = BufferedReader(FileReader(this.file))
     private var line: String? = ""
 
+    /**
+     * Splits the current line into an Array of Strings
+     */
     @Throws(IOException::class)
-    fun lineSplit(): Array<String> = line!!.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    fun lineSplit(): Array<String> = line!!.split(",".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
 
+    /**
+     * Gets the current line
+     */
     @Throws(IOException::class)
     fun line(): String? = line
 
+    /**
+     * Returns true if the file has next line otherwise false
+     */
     @Throws(IOException::class)
     fun hasNextLine(): Boolean {
         line = lineReader.readLine()
         return line != null
     }
 
+    /**
+     * Returns all the lines as Stream of Strings
+     */
     fun lines(): Stream<String> = lineReader.lines()
 }
